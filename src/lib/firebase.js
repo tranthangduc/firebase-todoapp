7086b678c5bd570caef6c4d3a3936acb444fe936
tranthangduc <tranthangduc0603@gmail.com>
2022-06-29 09:44:12 +0700

    Save Avatar by Storage

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-
+import 'firebase/compat/storage'
 const firebaseConfig = {
   apiKey: "AIzaSyAQME4Uz6sWp7E4n9OYjQgdeoxshaIJ0pc",
   authDomain: "todoapp-dded9.firebaseapp.com",
@@ -84,13 +84,25 @@ export const storeUserInfo = async (user) => {
   }
 }
 
-export const updateUser = async (user) => {
+export const updateUser = async (user, image) => {
   try {
     const userDoc = await firebase.firestore().collection("users").doc(user.id).get();
     if (userDoc.exists) {
-      await firebase.firestore().collection("users").doc(user.id).update({ ...userDoc.data() });
+      await firebase.firestore().collection("users").doc(user.id).update({ ...userDoc.data(), image: image });
     }
   } catch (err) {
     console.log(err);
   }
 }
+
+export const uploadImage = async (image) => {
+  const ref = firebase.storage().ref().child(`/images/${image.name}`);
+  let downloadUrl = "";
+  try {
+    await ref.put(image);
+    downloadUrl = await ref.getDownloadURL();
+  } catch (err) {
+    console.log(err);
+  }
+  return downloadUrl;
+};
